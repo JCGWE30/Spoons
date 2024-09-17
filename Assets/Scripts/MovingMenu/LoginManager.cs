d13674941a6a9ad086744b02029b9aabea097a7a
JCGWE30 <gwejc69@gmail.com>
2024-09-17 10:57:26 -0500

    More work

---
+++ Assets/Scripts/MovingMenu/LoginManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Core;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    [SerializeField] 
+    // Start is called before the first frame update
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
